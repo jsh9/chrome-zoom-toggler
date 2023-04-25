c8d248a0bd4c6ac16cf547adef8dde110413c126
jsh9 <25124332+jsh9@users.noreply.github.com>
2023-04-24 19:12:09 -0700

    Add pipeline, Prettier config, and format code

--- background.js
+++ background.js
@@ -35,11 +35,7 @@ chrome.action.onClicked.addListener((tab) => {
     console.log(`User-specified zoom levels: (${zoomLevel1}, ${zoomLevel2})`);
 
     // Toggle between the two user-defined zoom levels
-    let newZoom = determineNewZoomLevel(
-      currentZoom,
-      zoomLevel1,
-      zoomLevel2,
-    );
+    let newZoom = determineNewZoomLevel(currentZoom, zoomLevel1, zoomLevel2);
 
     // Apply the new zoom level to all open tabs
     setZoomLevelForAllTabs(currentZoom, newZoom);
@@ -50,8 +46,8 @@ chrome.action.onClicked.addListener((tab) => {
 // Function to set the zoom level for all open tabs
 function setZoomLevelForAllTabs(currentZoom, targetZoom) {
   console.log(
-    '>> Setting zoom level for all tabs from '
-    + `${currentZoom} to ${targetZoom}`
+    '>> Setting zoom level for all tabs from ' +
+      `${currentZoom} to ${targetZoom}`,
   );
   chrome.tabs.query({}, (tabs) => {
     tabs.forEach((tab) => {
@@ -64,18 +60,17 @@ function setZoomLevelForAllTabs(currentZoom, targetZoom) {
 
 // Apply the stored zoom level to all new tabs when they are created
 chrome.tabs.onCreated.addListener((tab) => {
-  console.log('------- New tab created -------')
+  console.log('------- New tab created -------');
   setZoomToTargetLevelInGivenTab(tab.id);
 });
 
 // Apply the stored zoom level to all updated tabs when they are refreshed
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  console.log('------- Tab refreshed -------')
+  console.log('------- Tab refreshed -------');
   if (changeInfo.status === 'complete') {
     setZoomToTargetLevelInGivenTab(tabId);
   }
 });
-
 
 function setZoomToTargetLevelInGivenTab(tabId) {
   chrome.tabs.getZoom(tabId, (currentZoomRaw) => {
@@ -85,19 +80,18 @@ function setZoomToTargetLevelInGivenTab(tabId) {
 
       console.log('>> Setting zoom level for current tab');
       console.log(
-        `   Current zoom: ${currentZoom}; target zoom: ${targetZoom}`
+        `   Current zoom: ${currentZoom}; target zoom: ${targetZoom}`,
       );
 
       if (areNumbersClose(currentZoom, targetZoom)) {
         console.log('Two zoom levels are close enough; no actions');
       } else {
-        console.log(`   Setting current zoom level to ${targetZoom}`)
+        console.log(`   Setting current zoom level to ${targetZoom}`);
         chrome.tabs.setZoom(tabId, targetZoom / 100);
       }
     });
   });
 }
-
 
 function determineNewZoomLevel(currentZoom, zoomLevel1, zoomLevel2) {
   let newZoomLevel = areNumbersClose(currentZoom, zoomLevel1)
@@ -106,7 +100,6 @@ function determineNewZoomLevel(currentZoom, zoomLevel1, zoomLevel2) {
 
   return newZoomLevel;
 }
-
 
 function areNumbersClose(a, b) {
   return Math.abs(a - b) < EPSILON;
--- options.js
+++ options.js
@@ -14,13 +14,16 @@ document.addEventListener('DOMContentLoaded', () => {
     const zoom1 = parseInt(document.getElementById('zoom1').value);
     const zoom2 = parseInt(document.getElementById('zoom2').value);
 
-    chrome.storage.sync.set({ [ZOOM_LEVEL_1_KEY]: zoom1, [ZOOM_LEVEL_2_KEY]: zoom2 }, () => {
-      // Display a message to the user to indicate that the zoom levels have been saved
-      const status = document.getElementById('status');
-      status.textContent = 'Zoom levels saved.';
-      setTimeout(() => {
-        status.textContent = '';
-      }, 2000);
-    });
+    chrome.storage.sync.set(
+      { [ZOOM_LEVEL_1_KEY]: zoom1, [ZOOM_LEVEL_2_KEY]: zoom2 },
+      () => {
+        // Display a message to the user to indicate that the zoom levels have been saved
+        const status = document.getElementById('status');
+        status.textContent = 'Zoom levels saved.';
+        setTimeout(() => {
+          status.textContent = '';
+        }, 2000);
+      },
+    );
   });
 });
